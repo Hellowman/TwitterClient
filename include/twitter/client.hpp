01e191ade10159c820948ef37cb54eff70321d88
Hellowman <Hellowman27@gmail.com>
2016-10-26 19:31:42 +0300

    Update client.hpp

--- include/twitter/client.hpp
+++ include/twitter/client.hpp
@@ -1,1 +1,20 @@
 
+#include <string>
+#include <map>
+
+
+namespace Twitter{
+
+ class Client{
+
+ public:
+
+   using dict_t = std::map<std::string, std::string>;
+
+   Client(dict_t settings);
+
+   auto check_connection() -> bool;
+
+ };
+
+}
